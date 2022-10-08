15d4605fa1669f5fdca64656810f63b41b4d1c0e
Pikanchik <114467599+Pikanchik@users.noreply.github.com>
2022-10-08 14:23:38 +0300

    complite raw add card in stack

--- Remembarroir4mobile/Assets/Scripts/AddCard.cs
+++ Remembarroir4mobile/Assets/Scripts/AddCard.cs
@@ -9,52 +9,93 @@ public class AddCard : MonoBehaviour
     public Button addCardButton;
 
     public GameObject DoneButton;
-    public GameObject addCardObj;
+    public GameObject addCardButtonObj;
     public GameObject removeCardObj;
     public GameObject openCardObj;
-    public GameObject AnwersinputFieldObj;
+    public GameObject AnswersinputFieldObj;
     public GameObject QuestioninputFieldObj;
+
     public List<Cards> cards = new List<Cards>();
 
+    private Vector3 QuestionStartPoint = new Vector3(0, 0.7f, 10);
+    private Vector3 QuestionFinishPoint = new Vector3(-1, 2, 10);
+    private Vector3 AnswerStartPoint = new Vector3(0, 0.8f, 10);
+    private Vector3 AnswerFinishPiont = new Vector3(1.3f, -2.2f, 10);
 
-
-    private Vector3 FinishPoint = new Vector3(-1, 2, 10);
-    private Vector3 FinishPoint_input = new Vector3(1.3f, -2.2f, 10);
 
     public void AddCardInStack()
     {
-        if (AnwersinputFieldObj.GetComponent<TMP_InputField>().text != null & QuestioninputFieldObj.GetComponent<TMP_InputField>().text != null)
+        if (AnswersinputFieldObj.GetComponent<TMP_InputField>().text != "" & QuestioninputFieldObj.GetComponent<TMP_InputField>().text != "")
         {
             Debug.Log("Input Detected");
-            cards.Add(new Cards(AnwersinputFieldObj.GetComponent<TMP_InputField>().text, QuestioninputFieldObj.GetComponent<TMP_InputField>().text));
-        }    
+            cards.Add(new Cards(AnswersinputFieldObj.GetComponent<TMP_InputField>().text, QuestioninputFieldObj.GetComponent<TMP_InputField>().text));
+            StartCoroutine(MoveObjNormalPosition());
+            DoneButton.SetActive(false);
+            addCardButton.interactable = true;
+            AnswersinputFieldObj.GetComponent<TMP_InputField>().text = "";
+            QuestioninputFieldObj.GetComponent<TMP_InputField>().text = "";
+        }
+
+        else if (QuestioninputFieldObj.GetComponent<TMP_InputField>().text == "")
+        {
+            openCardObj.GetComponent<Animation>().Play();
+            Debug.Log("Must write somefing...");
+        }
+
+        else if (AnswersinputFieldObj.GetComponent<TMP_InputField>().text == "")
+        {
+            AnswersinputFieldObj.GetComponent<Animation>().Play();
+            Debug.Log("Must write somefing...");
+        }
     }
 
     public void AddCardButton()
     {
         SetVisibleObj();
-        StartCoroutine(MoveObj());
+        StartCoroutine(MoveObjAddPsoition());
         DoneButton.SetActive(true);
-
     }
 
     public void SetVisibleObj()
     {
-        AnwersinputFieldObj.SetActive(true);
+        AnswersinputFieldObj.SetActive(true);
         addCardButton.interactable = false;
         openCardObj.GetComponent<Button>().interactable = false;
         removeCardObj.GetComponent<Button>().interactable = false;
         QuestioninputFieldObj.SetActive(true);
     }
 
-    public IEnumerator MoveObj()
+    public IEnumerator MoveObjAddPsoition()
     {
-        while(openCardObj.transform.position != FinishPoint & AnwersinputFieldObj.transform.position != FinishPoint_input)
+        int speed = 150;
+        float accuracy = 0.001f;
+
+        while ((QuestionFinishPoint - openCardObj.transform.position).sqrMagnitude > accuracy || (AnswerFinishPiont - AnswersinputFieldObj.transform.position).sqrMagnitude > accuracy)
         {
-            openCardObj.transform.position = Vector3.MoveTowards(openCardObj.transform.position, FinishPoint, Time.deltaTime * 150);
-            AnwersinputFieldObj.transform.position = Vector3.MoveTowards(AnwersinputFieldObj.transform.position, FinishPoint_input, Time.deltaTime * 150);
+            openCardObj.transform.position = Vector3.MoveTowards(openCardObj.transform.position, QuestionFinishPoint, Time.deltaTime * speed);
+            AnswersinputFieldObj.transform.position = Vector3.MoveTowards(AnswersinputFieldObj.transform.position, AnswerFinishPiont, Time.deltaTime * speed);
             yield return null;
         }
+        //Debug.Log("Corutine stoped");
+    }
+
+    public IEnumerator MoveObjNormalPosition()
+    {
+        int speed = 15;
+        float accuracy = 0.001f;
+        //Debug.Log("Coroutine started");
+
+        while ((QuestionStartPoint - openCardObj.transform.position).sqrMagnitude > accuracy || (AnswerStartPoint - AnswersinputFieldObj.transform.position).sqrMagnitude > accuracy)
+        {
+            openCardObj.transform.position = Vector3.MoveTowards(openCardObj.transform.position, QuestionStartPoint, Time.deltaTime * speed);
+            AnswersinputFieldObj.transform.position = Vector3.MoveTowards(AnswersinputFieldObj.transform.position, AnswerStartPoint, Time.deltaTime * speed);
+            yield return null;
+        }
+
+        AnswersinputFieldObj.SetActive(false);
+        QuestioninputFieldObj.SetActive(false);
+
+        //Debug.Log("Coroutine Stoped");
     }
 
     void Start()
@@ -65,10 +106,11 @@ public class AddCard : MonoBehaviour
     void Update()
     {
         //Debug.Log($"Вопрос {openCardObj.transform.position}\n\tОтвет {AnwersinputFieldObj.transform.position}");
-        if (cards[0] != null)
+        if (cards.Count != 0)
         {
             Debug.Log(cards[cards.Count - 1].questions);
         }
+        //Debug.Log(AnwersinputFieldObj.GetComponent<TMP_InputField>().text);
         
     }
 }
